0e30f88e673db625a22c569cfd57b90bd7f9d469
Parth <I_M_Parth>
2026-03-09 12:40:19 +0530

    create show action for billetto events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,16 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show]
+
   def index
     @events = Event.order(start_date: :asc)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_event
+    @event = Event.find_by!(id: params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  get "events/index"
+  # root "events#index"
+  resources :events, only: [:index, :show]
 end
